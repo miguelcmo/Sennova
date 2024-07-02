6623ab005ccdc2a0bc79c0f028fb3d366f2fbe97
Miguel Angel <miguel.carrillo.m@hotmail.com>
2024-07-02 17:23:19 -0500

    test

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -67,7 +67,7 @@ AppAsset::register($this);
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <div class="col-md-3 mb-2 mb-md-0">
         <a href="#" class="d-inline-flex link-body-emphasis text-decoration-none">
-          SERVISENA
+          SERVISENA DANIELA
         </a>
       </div>
 
