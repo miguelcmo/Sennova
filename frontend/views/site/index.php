d191f19e4424d02da81e02dfaa7599688aa00294
Miguel Angel <miguel.carrillo.m@hotmail.com>
2024-07-02 16:53:44 -0500

    Initial commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,103 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Servisena';
+?>
+<!--
+<div class="site-index">
+    <div class="p-5 mb-4 bg-transparent rounded-3">
+        <div class="container-fluid py-5 text-center">
+            <h1 class="display-4">Congratulations!</h1>
+            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
+            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+        </div>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
+-->
+<h1 class="visually-hidden">Heroes examples</h1>
+
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="" alt="Logo Servisena" width="72" height="57">
+    <h1 class="display-5 fw-bold text-body-emphasis">Bienvenidos al portal de investigación, innovación y desarrollo del Centro de Comercio del SENA, Regional Antioquia.</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Este hace parte integral del Sistema de Investigación, Desarrollo Tecnológico e Innovación (SENNOVA), el cual es un espacio dinámico para la promición de la innovación y los procesos de investigación en nuestro centro formativo.</p>
+        <!--
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        </div>
+        -->
+    </div>
+</div>
+
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="" class="d-block mx-lg-auto img-fluid" alt="imagen impulsar  articular formar" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <!--<h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>-->
+        <p class="lead">SennovaLab tiene como objeto presentar los proyectos que impulsan la innovación y el desarrollo en nuestra comunidad educativa, los cuales van desde investigaciones básicas, aplicadas, formativas y pedagógicas.</p>
+        <p class="lead">Proyectos que son ejemplos vivos de nuestro compromiso con la excelencia en la formación profesional integral de los aprendices y la respuesta pertinente al sector productivo.</p>
+        <!--
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+        </div>
+        -->
+      </div>
+    </div>
+</div>
+
+<div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold text-body-emphasis">PROYECTOS DESTACADOS</h1>
+    <p>slider fotos proyectos destacados</p>
+</div>
+
+<div class="px-4 py-5 my-5 text-left">
+    <h3 class="display-5 fw-bold text-body-emphasis">GALERIA FOTOGRÁFICA</h3>
+    <p>Galeria imagenes proyectos Sennova</p>
+</div>
+
+<div class="px-4 py-5 my-5 text-left">
+    <h3 class="display-5 fw-bold text-body-emphasis">NUESTRO EQUIPO DE TRABAJO</h3>
+    <p>Logos SENA</p>
+</div>
